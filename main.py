7b04624a46f1fd7561b78fcc59f5f95efe0815dc
Carol Zhang <zhang.carol19@gmail.com>
2021-01-27 10:07:34 -0800

    replace parameters with arguments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from grapher import Grapher
 def main():
 
     # parameters: filename of your top output file, where your top output text file is saved
-    tp = TopParser(filename=str(sys.argv[0]), dir_text=str(sys.argv[1])) 
+    tp = TopParser(filename=str(sys.argv[1]), dir_text=str(sys.argv[2])) 
 
     text = tp.parseText()
     data = tp.fillDict(text)
@@ -24,7 +24,7 @@ def main():
     # parameters (leave data and summary parameter): 
     #       - filename that you want your xlsx file to be, 
     #       - where you want your xlsx file to be saved
-    g = Grapher(data=data, summary=summary, filename=str(sys.argv[0]), dir_xlsx=str(sys.argv[1]))
+    g = Grapher(data=data, summary=summary, filename=str(sys.argv[1]), dir_xlsx=str(sys.argv[2]))
 
     df1, df_np = g.convertToNumpy()
     g.createPlot(df_np)
